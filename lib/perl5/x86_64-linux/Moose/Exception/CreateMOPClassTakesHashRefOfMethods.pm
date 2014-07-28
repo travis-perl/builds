51ad5b8416378f2a7af328184f32155ea94cc8da
Graham Knop <haarg@haarg.org>
2014-07-28 11:34:06 +0000

    local::lib cat for Perl 5.8.9 built for heads/master-0-ga94da75

---
+++ lib/perl5/x86_64-linux/Moose/Exception/CreateMOPClassTakesHashRefOfMethods.pm
@@ -0,0 +1,14 @@
+package Moose::Exception::CreateMOPClassTakesHashRefOfMethods;
+BEGIN {
+  $Moose::Exception::CreateMOPClassTakesHashRefOfMethods::AUTHORITY = 'cpan:STEVAN';
+}
+$Moose::Exception::CreateMOPClassTakesHashRefOfMethods::VERSION = '2.1210';
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::RoleForCreateMOPClass';
+
+sub _build_message {
+    "You must pass an HASH ref of methods";
+}
+
+1;
---
+++ lib/perl5/x86_64-linux/Moose/Exception/WrapTakesACodeRefToBless.pm
@@ -0,0 +1,27 @@
+package Moose::Exception::WrapTakesACodeRefToBless;
+BEGIN {
+  $Moose::Exception::WrapTakesACodeRefToBless::AUTHORITY = 'cpan:STEVAN';
+}
+$Moose::Exception::WrapTakesACodeRefToBless::VERSION = '2.1210';
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::ParamsHash';
+
+has 'code' => (
+    is       => 'ro',
+    isa      => 'Any',
+    required => 1
+);
+
+has 'class' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+sub _build_message {
+    my $self = shift;
+    "You must supply a CODE reference to bless, not (" . ( $self->code ? $self->code : 'undef' ) . ")";
+}
+
+1;
