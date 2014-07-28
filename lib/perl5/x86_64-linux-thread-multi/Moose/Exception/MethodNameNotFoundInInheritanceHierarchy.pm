9f75f9cc2e0d6361ecde0379202cb0ab39d95f8c
Graham Knop <haarg@haarg.org>
2014-07-28 12:19:45 +0000

    local::lib moose for Perl 5.16.3-thr built for heads/master-0-ga94da75

---
+++ lib/perl5/x86_64-linux-thread-multi/Moose/Exception/MethodNameNotFoundInInheritanceHierarchy.pm
@@ -0,0 +1,21 @@
+package Moose::Exception::MethodNameNotFoundInInheritanceHierarchy;
+BEGIN {
+  $Moose::Exception::MethodNameNotFoundInInheritanceHierarchy::AUTHORITY = 'cpan:STEVAN';
+}
+$Moose::Exception::MethodNameNotFoundInInheritanceHierarchy::VERSION = '2.1210';
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::Class';
+
+has 'method_name' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+sub _build_message {
+    my $self = shift;
+    "The method '".$self->method_name."' was not found in the inheritance hierarchy for ".$self->class_name;
+}
+
+1;
