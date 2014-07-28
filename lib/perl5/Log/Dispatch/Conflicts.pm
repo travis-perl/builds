b12a3200113f21d579be77814abf6a096ef9960b
Graham Knop <haarg@haarg.org>
2014-07-28 12:46:30 +0000

    local::lib dzil for Perl 5.18.2 built for heads/master-0-ga94da75

---
+++ lib/perl5/Log/Dispatch/Conflicts.pm
@@ -0,0 +1,43 @@
+package # hide from PAUSE
+    Log::Dispatch::Conflicts;
+
+use strict;
+use warnings;
+
+use Dist::CheckConflicts
+    -dist      => 'Log::Dispatch',
+    -conflicts => {
+        'Log::Dispatch::File::Stamped' => '0.10',
+    },
+
+;
+
+1;
+
+# ABSTRACT: Provide information on conflicts for Log::Dispatch
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Log::Dispatch::Conflicts - Provide information on conflicts for Log::Dispatch
+
+=head1 VERSION
+
+version 2.41
+
+=head1 AUTHOR
+
+Dave Rolsky <autarch@urth.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2013 by Dave Rolsky.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+=cut
---
+++ lib/perl5/x86_64-linux/Moose/Exception/CannotCallAnAbstractBaseMethod.pm
@@ -0,0 +1,20 @@
+package Moose::Exception::CannotCallAnAbstractBaseMethod;
+BEGIN {
+  $Moose::Exception::CannotCallAnAbstractBaseMethod::AUTHORITY = 'cpan:STEVAN';
+}
+$Moose::Exception::CannotCallAnAbstractBaseMethod::VERSION = '2.1210';
+use Moose;
+extends 'Moose::Exception';
+
+has 'package_name' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+sub _build_message {
+    my $self = shift;
+    $self->package_name. " is an abstract base class, you must provide a constructor.";
+}
+
+1;
