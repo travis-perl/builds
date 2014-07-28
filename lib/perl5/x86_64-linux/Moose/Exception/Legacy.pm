a7548e343149d4e47d71dc2377aacaee3af4c512
Graham Knop <haarg@haarg.org>
2014-07-28 11:54:27 +0000

    local::lib moose for Perl 5.10.1 built for heads/master-0-ga94da75

---
+++ lib/perl5/x86_64-linux/Moose/Exception/Legacy.pm
@@ -0,0 +1,9 @@
+package Moose::Exception::Legacy;
+BEGIN {
+  $Moose::Exception::Legacy::AUTHORITY = 'cpan:STEVAN';
+}
+$Moose::Exception::Legacy::VERSION = '2.1210';
+use Moose;
+extends 'Moose::Exception';
+
+1;
