2c64157000afa845078c8a7213e93d9424ca3ccb
Graham Knop <haarg@haarg.org>
2014-07-28 12:45:01 +0000

    local::lib moose for Perl 5.18.2-thr built for heads/master-0-ga94da75

---
+++ lib/perl5/x86_64-linux-thread-multi/Moose/Exception/CannotGenerateInlineConstraint.pm
@@ -0,0 +1,31 @@
+package Moose::Exception::CannotGenerateInlineConstraint;
+BEGIN {
+  $Moose::Exception::CannotGenerateInlineConstraint::AUTHORITY = 'cpan:STEVAN';
+}
+$Moose::Exception::CannotGenerateInlineConstraint::VERSION = '2.1210';
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::TypeConstraint';
+
+has 'parameterizable_type_object_name' => (
+    is            => 'ro',
+    isa           => 'Str',
+    required      => 1,
+    documentation => "This attribute can be used for fetching parameterizable type constraint(Moose::Meta::TypeConstraint::Parameterizable):\n".
+                     "    my \$type_constraint =  Moose::Util::TypeConstraints::find_type_constraint( \$exception->type_name );\n",
+);
+
+has 'value' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+sub _build_message {
+    my $self = shift;
+    my $type = $self->type_name;
+
+    return "Can't generate an inline constraint for $type, since none was defined";
+}
+
+1;
