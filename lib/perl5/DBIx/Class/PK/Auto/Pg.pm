7ed638fd766a10889dcd117deb6bdcd57221c4e6
Graham Knop <haarg@haarg.org>
2014-07-28 12:08:26 +0000

    local::lib dbic for Perl 5.12.5-thr built for heads/master-0-ga94da75

---
+++ lib/perl5/DBIx/Class/PK/Auto/Pg.pm
@@ -0,0 +1,29 @@
+package # hide package from pause
+  DBIx::Class::PK::Auto::Pg;
+
+use strict;
+use warnings;
+
+use base qw/DBIx::Class/;
+
+__PACKAGE__->load_components(qw/PK::Auto/);
+
+1;
+
+=head1 NAME
+
+DBIx::Class::PK::Auto::Pg - (DEPRECATED) Automatic primary key class for Pg
+
+=head1 SYNOPSIS
+
+Just load PK::Auto instead; auto-inc is now handled by Storage.
+
+=head1 AUTHOR AND CONTRIBUTORS
+
+See L<AUTHOR|DBIx::Class/AUTHOR> and L<CONTRIBUTORS|DBIx::Class/CONTRIBUTORS> in DBIx::Class
+
+=head1 LICENSE
+
+You may distribute this code under the same terms as Perl itself.
+
+=cut
