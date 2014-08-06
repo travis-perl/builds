f4517b19d7125a16e709e0a8aced26eaeae6a13d
Graham Knop <haarg@haarg.org>
2014-08-06 17:17:23 +0000

    local::lib dzil for Perl 5.8.8 built for heads/master-0-g0e2b730

--- lib/perl5/DateTime/TimeZone/OlsonDB/Change.pm
+++ lib/perl5/DateTime/TimeZone/OlsonDB/Change.pm
@@ -1,5 +1,5 @@
 package DateTime::TimeZone::OlsonDB::Change;
-$DateTime::TimeZone::OlsonDB::Change::VERSION = '1.71';
+$DateTime::TimeZone::OlsonDB::Change::VERSION = '1.72';
 use strict;
 use warnings;
 
