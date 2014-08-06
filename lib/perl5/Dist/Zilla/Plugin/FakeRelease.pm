98d3054cd38209cd25cfc57625d757ee5f84990c
Graham Knop <haarg@haarg.org>
2014-08-06 17:50:40 +0000

    local::lib dzil for Perl 5.10.1-thr built for heads/master-0-g0e2b730

--- lib/perl5/Dist/Zilla/Plugin/FakeRelease.pm
+++ lib/perl5/Dist/Zilla/Plugin/FakeRelease.pm
@@ -1,6 +1,6 @@
 package Dist::Zilla::Plugin::FakeRelease;
 # ABSTRACT: fake plugin to test release
-$Dist::Zilla::Plugin::FakeRelease::VERSION = '5.019';
+$Dist::Zilla::Plugin::FakeRelease::VERSION = '5.020';
 use Moose;
 with 'Dist::Zilla::Role::Releaser';
 
@@ -71,7 +71,7 @@ Dist::Zilla::Plugin::FakeRelease - fake plugin to test release
 
 =head1 VERSION
 
-version 5.019
+version 5.020
 
 =head1 SYNOPSIS
 
--- lib/perl5/Test/DZil.pm
+++ lib/perl5/Test/DZil.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package Test::DZil;
 # ABSTRACT: tools for testing Dist::Zilla plugins
-$Test::DZil::VERSION = '5.019';
+$Test::DZil::VERSION = '5.020';
 use Dist::Zilla::Tester;
 use Params::Util qw(_HASH0);
 use JSON 2;
@@ -240,7 +240,7 @@ Test::DZil - tools for testing Dist::Zilla plugins
 
 =head1 VERSION
 
-version 5.019
+version 5.020
 
 =head1 DESCRIPTION
 
