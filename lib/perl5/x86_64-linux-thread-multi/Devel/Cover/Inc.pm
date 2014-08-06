46c4df9cb53be16208378c66a9aae2eab34b6b77
Graham Knop <haarg@haarg.org>
2014-08-06 17:20:33 +0000

    local::lib cover for Perl 5.8.9-thr built for heads/master-0-g0e2b730

--- lib/perl5/x86_64-linux-thread-multi/Devel/Cover/Inc.pm
+++ lib/perl5/x86_64-linux-thread-multi/Devel/Cover/Inc.pm
@@ -14,7 +14,7 @@ use warnings;
 
 our $VERSION      = "1.15";
 our $Perl_version = '5.008009';
-our $Base         = '/home/travis/.cpanm/work/1406546323.30599/Devel-Cover-1.15';
+our $Base         = '/home/travis/.cpanm/work/1407344614.30103/Devel-Cover-1.15';
 our @Inc          = qw( . /home/travis/perl5/perlbrew/perls/5.8.9-thr/lib/5.8.9 /home/travis/perl5/perlbrew/perls/5.8.9-thr/lib/5.8.9/x86_64-linux-thread-multi /home/travis/perl5/perlbrew/perls/5.8.9-thr/lib/site_perl/5.8.9 /home/travis/perl5/perlbrew/perls/5.8.9-thr/lib/site_perl/5.8.9/x86_64-linux-thread-multi );
 chomp (our $Perl  = <<'EOV');  # Careful with \\ in the path
 /home/travis/perl5/perlbrew/perls/5.8.9-thr/bin/perl
