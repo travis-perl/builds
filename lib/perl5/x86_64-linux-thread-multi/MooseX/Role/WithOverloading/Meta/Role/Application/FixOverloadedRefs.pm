09e91672f3f7789278dfd6e5e9cc52bf89322185
Graham Knop <haarg@haarg.org>
2014-08-06 17:20:32 +0000

    local::lib cat for Perl 5.8.9-thr built for heads/master-0-g0e2b730

--- lib/perl5/x86_64-linux-thread-multi/MooseX/Role/WithOverloading/Meta/Role/Application/FixOverloadedRefs.pm
+++ lib/perl5/x86_64-linux-thread-multi/MooseX/Role/WithOverloading/Meta/Role/Application/FixOverloadedRefs.pm
@@ -2,11 +2,8 @@ package MooseX::Role::WithOverloading::Meta::Role::Application::FixOverloadedRef
 BEGIN {
   $MooseX::Role::WithOverloading::Meta::Role::Application::FixOverloadedRefs::AUTHORITY = 'cpan:FLORA';
 }
-{
-  $MooseX::Role::WithOverloading::Meta::Role::Application::FixOverloadedRefs::VERSION = '0.13';
-}
 # ABSTRACT: Fix up magic when applying roles to instances with magic on old perls
-
+$MooseX::Role::WithOverloading::Meta::Role::Application::FixOverloadedRefs::VERSION = '0.14';
 use Moose::Role;
 use namespace::autoclean;
 
@@ -16,6 +13,13 @@ if ($] < 5.008009) {
     };
 }
 
+#pod =begin Pod::Coverage
+#pod
+#pod reset_amagic
+#pod
+#pod =end Pod::Coverage
+#pod
+#pod =cut
 
 1;
 
@@ -23,11 +27,15 @@ __END__
 
 =pod
 
-=encoding utf-8
+=encoding UTF-8
 
 =head1 NAME
 
 MooseX::Role::WithOverloading::Meta::Role::Application::FixOverloadedRefs - Fix up magic when applying roles to instances with magic on old perls
+
+=head1 VERSION
+
+version 0.14
 
 =for Pod::Coverage reset_amagic
 
@@ -47,7 +55,7 @@ Tomas Doran <bobtfish@bobtfish.net>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2013 by Florian Ragwitz.
+This software is copyright (c) 2014 by Florian Ragwitz.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
