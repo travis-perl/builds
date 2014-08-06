2f407eef9f56a46760895fd76a6abe5a31ccea26
Graham Knop <haarg@haarg.org>
2014-08-06 17:22:16 +0000

    Perl 5.10.0 built for heads/master-0-g0e2b730

--- lib/5.10.0/x86_64-linux/CORE/config.h
+++ lib/5.10.0/x86_64-linux/CORE/config.h
@@ -13,9 +13,9 @@
 /*
  * Package name      : perl5
  * Source directory  : .
- * Configuration time: Mon Jul 28 11:13:16 UTC 2014
+ * Configuration time: Wed Aug  6 16:55:15 UTC 2014
  * Configured by     : travis
- * Target system     : linux testing-worker-linux-9-1-10830-linux-14-31030833 2.6.32-042stab090.5 #1 smp sat jun 21 00:15:09 msk 2014 x86_64 x86_64 x86_64 gnulinux 
+ * Target system     : linux testing-worker-linux-2-1-13765-linux-15-31828057 2.6.32-042stab090.5 #1 smp sat jun 21 00:15:09 msk 2014 x86_64 x86_64 x86_64 gnulinux 
  */
 
 #ifndef _config_h_
