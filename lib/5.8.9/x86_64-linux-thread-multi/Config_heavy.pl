47358f299b9c64967d5ad6f47c8c5573b1c9171d
Graham Knop <haarg@haarg.org>
2014-08-06 23:02:10 +0000

    Perl 5.8.9-thr built for heads/master-0-gab85784

--- lib/5.8.9/x86_64-linux-thread-multi/Config_heavy.pl
+++ lib/5.8.9/x86_64-linux-thread-multi/Config_heavy.pl
@@ -14,9 +14,9 @@ use strict;
 #
 ## Package name      : perl5
 ## Source directory  : .
-## Configuration time: Wed Aug  6 16:53:43 UTC 2014
+## Configuration time: Wed Aug  6 22:38:22 UTC 2014
 ## Configured by     : travis
-## Target system     : linux testing-worker-linux-9-2-2211-linux-9-31828056 2.6.32-042stab090.5 #1 smp sat jun 21 00:15:09 msk 2014 x86_64 x86_64 x86_64 gnulinux 
+## Target system     : linux testing-worker-linux-7-2-32004-linux-17-31859760 2.6.32-042stab090.5 #1 smp sat jun 21 00:15:09 msk 2014 x86_64 x86_64 x86_64 gnulinux 
 #
 ## Configure command line arguments.
 #PERL_PATCHLEVEL=
@@ -125,8 +125,8 @@ ccname='gcc'
 ccsymbols=''
 ccversion=''
 cf_by='travis'
-cf_email='travis@testing-worker-linux-9-2-2211-linux-9-31828056.c45665.blueboxgrid.com'
-cf_time='Wed Aug  6 16:53:43 UTC 2014'
+cf_email='travis@testing-worker-linux-7-2-32004-linux-17-31859760.c45665.blueboxgrid.com'
+cf_time='Wed Aug  6 22:38:22 UTC 2014'
 chgrp=''
 chmod='chmod'
 chown=''
@@ -867,8 +867,8 @@ multiarch='undef'
 mv=''
 myarchname='x86_64-linux'
 mydomain='.c45665.blueboxgrid.com'
-myhostname='testing-worker-linux-9-2-2211-linux-9-31828056'
-myuname='linux testing-worker-linux-9-2-2211-linux-9-31828056 2.6.32-042stab090.5 #1 smp sat jun 21 00:15:09 msk 2014 x86_64 x86_64 x86_64 gnulinux '
+myhostname='testing-worker-linux-7-2-32004-linux-17-31859760'
+myuname='linux testing-worker-linux-7-2-32004-linux-17-31859760 2.6.32-042stab090.5 #1 smp sat jun 21 00:15:09 msk 2014 x86_64 x86_64 x86_64 gnulinux '
 n='-n'
 need_va_copy='define'
 netdb_hlen_type='size_t'
@@ -905,7 +905,7 @@ path_sep=':'
 perl5='/usr/bin/perl'
 perl=''
 perl_patchlevel=''
-perladmin='travis@testing-worker-linux-9-2-2211-linux-9-31828056.c45665.blueboxgrid.com'
+perladmin='travis@testing-worker-linux-7-2-32004-linux-17-31859760.c45665.blueboxgrid.com'
 perllibs='-lnsl -ldl -lm -lcrypt -lutil -lpthread -lc -lgdbm_compat'
 perlpath='/home/travis/perl5/perlbrew/perls/5.8.9-thr/bin/perl'
 pg='pg'
