134b2c4b0e888a9890be05826b89089fd11ed2d7
Graham Knop <haarg@haarg.org>
2014-08-06 18:40:55 +0000

    local::lib moose for Perl 5.20.0 built for heads/master-0-g0e2b730

--- lib/perl5/Module/Build/ConfigData.pm
+++ lib/perl5/Module/Build/ConfigData.pm
@@ -167,44 +167,44 @@ do{ my $x = [
        {},
        {},
        {
-         'inc_bundling_support' => {
-                                     'description' => 'Bundle Module::Build in inc/',
-                                     'requires' => {
-                                                     'ExtUtils::Install' => '1.54',
-                                                     'ExtUtils::Installed' => '1.999'
-                                                   }
-                                   },
-         'license_creation' => {
-                                 'requires' => {
-                                                 'Software::License' => '0.103009'
-                                               },
-                                 'description' => 'Create licenses automatically in distributions'
-                               },
+         'dist_authoring' => {
+                               'description' => 'Create new distributions',
+                               'requires' => {
+                                               'Archive::Tar' => '1.09'
+                                             },
+                               'recommends' => {
+                                                 'Module::Signature' => '0.21',
+                                                 'Pod::Readme' => '0.04'
+                                               }
+                             },
          'HTML_support' => {
-                             'description' => 'Create HTML documentation',
                              'requires' => {
                                              'Pod::Html' => 0
-                                           }
+                                           },
+                             'description' => 'Create HTML documentation'
                            },
          'PPM_support' => {
                             'description' => 'Generate PPM files for distributions'
                           },
+         'inc_bundling_support' => {
+                                     'description' => 'Bundle Module::Build in inc/',
+                                     'requires' => {
+                                                     'ExtUtils::Installed' => '1.999',
+                                                     'ExtUtils::Install' => '1.54'
+                                                   }
+                                   },
+         'license_creation' => {
+                                 'description' => 'Create licenses automatically in distributions',
+                                 'requires' => {
+                                                 'Software::License' => '0.103009'
+                                               }
+                               },
          'manpage_support' => {
-                                'description' => 'Create Unix man pages',
                                 'requires' => {
                                                 'Pod::Man' => 0
-                                              }
-                              },
-         'dist_authoring' => {
-                               'requires' => {
-                                               'Archive::Tar' => '1.09'
-                                             },
-                               'description' => 'Create new distributions',
-                               'recommends' => {
-                                                 'Pod::Readme' => '0.04',
-                                                 'Module::Signature' => '0.21'
-                                               }
-                             }
+                                              },
+                                'description' => 'Create Unix man pages'
+                              }
        }
      ];
 $x; }
