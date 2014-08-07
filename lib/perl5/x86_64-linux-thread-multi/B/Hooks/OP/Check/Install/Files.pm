171eebddf30cca2cca735c4f4d5f13db23841ca1
Graham Knop <haarg@haarg.org>
2014-08-07 00:09:37 +0000

    local::lib dzil for Perl 5.18.2-thr built for heads/master-0-gab85784

--- lib/perl5/x86_64-linux-thread-multi/B/Hooks/OP/Check/Install/Files.pm
+++ lib/perl5/x86_64-linux-thread-multi/B/Hooks/OP/Check/Install/Files.pm
@@ -1,10 +1,10 @@
 package B::Hooks::OP::Check::Install::Files;
 
 $self = {
-          'deps' => [],
-          'libs' => '',
           'typemaps' => [],
-          'inc' => ''
+          'inc' => '',
+          'deps' => [],
+          'libs' => ''
         };
 
 
