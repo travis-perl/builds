362079274d17541a235772992e87f428e3b95460
Graham Knop <haarg@haarg.org>
2014-08-07 00:01:40 +0000

    local::lib cat for Perl 5.18.1-thr built for heads/master-0-gab85784

--- lib/perl5/File/ChangeNotify/ConfigData.pm
+++ lib/perl5/File/ChangeNotify/ConfigData.pm
@@ -168,18 +168,18 @@ do{ my $x = [
        {},
        {},
        {
-         'Inotify' => {
-                        'description' => 'Inotify support',
-                        'requires' => {
-                                        'Linux::Inotify2' => '1.2'
-                                      }
-                      },
          'KQueue' => {
                        'requires' => {
                                        'IO::KQueue' => 0
                                      },
                        'description' => 'KQueue support'
-                     }
+                     },
+         'Inotify' => {
+                        'requires' => {
+                                        'Linux::Inotify2' => '1.2'
+                                      },
+                        'description' => 'Inotify support'
+                      }
        }
      ];
 $x; }
