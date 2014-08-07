1350fb8eb5233640381ec2e7fc5522a3bd303fea
Graham Knop <haarg@haarg.org>
2014-08-07 00:10:36 +0000

    local::lib dbic for Perl 5.20.0-thr built for heads/master-0-gab85784

--- lib/perl5/Module/Build/ConfigData.pm
+++ lib/perl5/Module/Build/ConfigData.pm
@@ -167,25 +167,26 @@ do{ my $x = [
        {},
        {},
        {
-         'PPM_support' => {
-                            'description' => 'Generate PPM files for distributions'
-                          },
-         'HTML_support' => {
-                             'description' => 'Create HTML documentation',
-                             'requires' => {
-                                             'Pod::Html' => 0
-                                           }
-                           },
          'manpage_support' => {
-                                'description' => 'Create Unix man pages',
                                 'requires' => {
                                                 'Pod::Man' => 0
-                                              }
+                                              },
+                                'description' => 'Create Unix man pages'
                               },
+         'dist_authoring' => {
+                               'recommends' => {
+                                                 'Module::Signature' => '0.21',
+                                                 'Pod::Readme' => '0.04'
+                                               },
+                               'requires' => {
+                                               'Archive::Tar' => '1.09'
+                                             },
+                               'description' => 'Create new distributions'
+                             },
          'inc_bundling_support' => {
                                      'requires' => {
-                                                     'ExtUtils::Install' => '1.54',
-                                                     'ExtUtils::Installed' => '1.999'
+                                                     'ExtUtils::Installed' => '1.999',
+                                                     'ExtUtils::Install' => '1.54'
                                                    },
                                      'description' => 'Bundle Module::Build in inc/'
                                    },
@@ -195,16 +196,15 @@ do{ my $x = [
                                                  'Software::License' => '0.103009'
                                                }
                                },
-         'dist_authoring' => {
-                               'requires' => {
-                                               'Archive::Tar' => '1.09'
-                                             },
-                               'recommends' => {
-                                                 'Pod::Readme' => '0.04',
-                                                 'Module::Signature' => '0.21'
-                                               },
-                               'description' => 'Create new distributions'
-                             }
+         'PPM_support' => {
+                            'description' => 'Generate PPM files for distributions'
+                          },
+         'HTML_support' => {
+                             'description' => 'Create HTML documentation',
+                             'requires' => {
+                                             'Pod::Html' => 0
+                                           }
+                           }
        }
      ];
 $x; }
