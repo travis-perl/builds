91e5cfef10d741bd16a1016738021c08b09f5b2c
Graham Knop <haarg@haarg.org>
2014-09-23 13:56:46 +0000

    Perl 5.16.3-thr built for heads/master-0-gbc9bf91

--- lib/5.16.3/x86_64-linux-thread-multi/CORE/config.h
+++ lib/5.16.3/x86_64-linux-thread-multi/CORE/config.h
@@ -9,9 +9,9 @@
 
 /* Package name      : perl5
  * Source directory  : .
- * Configuration time: Wed Aug  6 23:30:49 UTC 2014
+ * Configuration time: Tue Sep 23 13:35:58 UTC 2014
  * Configured by     : travis
- * Target system     : linux testing-worker-linux-3-1-15179-linux-3-31859770 2.6.32-042stab090.5 #1 smp sat jun 21 00:15:09 msk 2014 x86_64 x86_64 x86_64 gnulinux 
+ * Target system     : linux testing-worker-linux-5-1-1654-linux-11-36040034 2.6.32-042stab090.5 #1 smp sat jun 21 00:15:09 msk 2014 x86_64 x86_64 x86_64 gnulinux 
  */
 
 #ifndef _config_h_
