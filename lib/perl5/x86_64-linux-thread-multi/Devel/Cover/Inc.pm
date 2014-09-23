504b83742a215f7a1246eeaff6b2d1b5fb633608
Graham Knop <haarg@haarg.org>
2014-09-23 13:29:25 +0000

    local::lib cover for Perl 5.10.1-thr built for heads/master-0-gbc9bf91

--- lib/perl5/x86_64-linux-thread-multi/Devel/Cover/Inc.pm
+++ lib/perl5/x86_64-linux-thread-multi/Devel/Cover/Inc.pm
@@ -12,10 +12,10 @@ package Devel::Cover::Inc;
 use strict;
 use warnings;
 
-our $VERSION      = "1.15";
+our $VERSION      = "1.17";
 our $Perl_version = '5.010001';
-our $Base         = '/home/travis/.cpanm/work/1407366754.29805/Devel-Cover-1.15';
-our @Inc          = qw( . /home/travis/perl5/perlbrew/perls/5.10.1-thr/lib/5.10.1 /home/travis/perl5/perlbrew/perls/5.10.1-thr/lib/5.10.1/x86_64-linux-thread-multi /home/travis/perl5/perlbrew/perls/5.10.1-thr/lib/site_perl/5.10.1 /home/travis/perl5/perlbrew/perls/5.10.1-thr/lib/site_perl/5.10.1/x86_64-linux-thread-multi );
+our $Base         = '/home/travis/.cpanm/work/1411478196.29027/Devel-Cover-1.17';
+our @Inc          = qw( . /home/travis/.perlbrew/libs/5.10.1-thr@cover/lib/perl5 /home/travis/.perlbrew/libs/5.10.1-thr@cover/lib/perl5/x86_64-linux-thread-multi /home/travis/perl5/perlbrew/perls/5.10.1-thr/lib/5.10.1 /home/travis/perl5/perlbrew/perls/5.10.1-thr/lib/5.10.1/x86_64-linux-thread-multi /home/travis/perl5/perlbrew/perls/5.10.1-thr/lib/site_perl/5.10.1 /home/travis/perl5/perlbrew/perls/5.10.1-thr/lib/site_perl/5.10.1/x86_64-linux-thread-multi );
 chomp (our $Perl  = <<'EOV');  # Careful with \\ in the path
 /home/travis/perl5/perlbrew/perls/5.10.1-thr/bin/perl
 EOV
