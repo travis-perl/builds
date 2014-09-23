bced051b5b941f8c66c2b0916f6d7e8020799fac
Graham Knop <haarg@haarg.org>
2014-09-23 13:38:56 +0000

    local::lib cover for Perl 5.12.5 built for heads/master-0-gbc9bf91

--- lib/perl5/x86_64-linux/Devel/Cover/Inc.pm
+++ lib/perl5/x86_64-linux/Devel/Cover/Inc.pm
@@ -12,10 +12,10 @@ package Devel::Cover::Inc;
 use strict;
 use warnings;
 
-our $VERSION      = "1.15";
+our $VERSION      = "1.17";
 our $Perl_version = '5.012005';
-our $Base         = '/home/travis/.cpanm/work/1407366821.2515/Devel-Cover-1.15';
-our @Inc          = qw( . /home/travis/perl5/perlbrew/perls/5.12.5/lib/5.12.5 /home/travis/perl5/perlbrew/perls/5.12.5/lib/5.12.5/x86_64-linux /home/travis/perl5/perlbrew/perls/5.12.5/lib/site_perl/5.12.5 /home/travis/perl5/perlbrew/perls/5.12.5/lib/site_perl/5.12.5/x86_64-linux );
+our $Base         = '/home/travis/.cpanm/work/1411478572.1760/Devel-Cover-1.17';
+our @Inc          = qw( . /home/travis/.perlbrew/libs/5.12.5@cover/lib/perl5 /home/travis/.perlbrew/libs/5.12.5@cover/lib/perl5/x86_64-linux /home/travis/perl5/perlbrew/perls/5.12.5/lib/5.12.5 /home/travis/perl5/perlbrew/perls/5.12.5/lib/5.12.5/x86_64-linux /home/travis/perl5/perlbrew/perls/5.12.5/lib/site_perl/5.12.5 /home/travis/perl5/perlbrew/perls/5.12.5/lib/site_perl/5.12.5/x86_64-linux );
 chomp (our $Perl  = <<'EOV');  # Careful with \\ in the path
 /home/travis/perl5/perlbrew/perls/5.12.5/bin/perl
 EOV
