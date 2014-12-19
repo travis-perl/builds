d2c849d4c60b6ec7df79a82eae405b054389381d
Graham Knop <haarg@haarg.org>
2014-12-19 23:03:31 +0000

    local::lib cat for Perl 5.18.1-thr built for heads/master-0-gb7145cc

--- lib/perl5/x86_64-linux-thread-multi/Moose/Exception/CannotFindType.pm
+++ lib/perl5/x86_64-linux-thread-multi/Moose/Exception/CannotFindType.pm
@@ -1,5 +1,6 @@
 package Moose::Exception::CannotFindType;
-$Moose::Exception::CannotFindType::VERSION = '2.1212';
+our $VERSION = '2.1403';
+
 use Moose;
 extends 'Moose::Exception';
 
--- lib/perl5/x86_64-linux-thread-multi/Moose/Meta/Method/Accessor/Native/Array/grep.pm
+++ lib/perl5/x86_64-linux-thread-multi/Moose/Meta/Method/Accessor/Native/Array/grep.pm
@@ -1,5 +1,6 @@
 package Moose::Meta::Method::Accessor::Native::Array::grep;
-$Moose::Meta::Method::Accessor::Native::Array::grep::VERSION = '2.1212';
+our $VERSION = '2.1403';
+
 use strict;
 use warnings;
 
