0843f707cc0e16fceab922a8ddc1e145b64798fe
Graham Knop <haarg@haarg.org>
2014-12-19 23:02:23 +0000

    local::lib cover for Perl 5.18.1 built for heads/master-0-gb7145cc

--- lib/perl5/x86_64-linux/Devel/Cover/Inc.pm
+++ lib/perl5/x86_64-linux/Devel/Cover/Inc.pm
@@ -14,7 +14,7 @@ use warnings;
 
 our $VERSION      = "1.17";
 our $Perl_version = '5.018001';
-our $Base         = '/home/travis/.cpanm/work/1411480012.1692/Devel-Cover-1.17';
+our $Base         = '/home/travis/.cpanm/work/1419029501.3447/Devel-Cover-1.17';
 our @Inc          = qw( . /home/travis/perl5/perlbrew/perls/5.18.1/lib/5.18.1 /home/travis/perl5/perlbrew/perls/5.18.1/lib/5.18.1/x86_64-linux /home/travis/perl5/perlbrew/perls/5.18.1/lib/site_perl/5.18.1 /home/travis/perl5/perlbrew/perls/5.18.1/lib/site_perl/5.18.1/x86_64-linux );
 chomp (our $Perl  = <<'EOV');  # Careful with \\ in the path
 /home/travis/perl5/perlbrew/perls/5.18.1/bin/perl
