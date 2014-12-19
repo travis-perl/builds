39ddf9e4018540ba6a41888b7a9997c032e271d2
Graham Knop <haarg@haarg.org>
2014-12-19 23:22:29 +0000

    local::lib moo for Perl 5.20.0-thr built for heads/master-0-gb7145cc

--- lib/perl5/Module/Build/ConfigData.pm
+++ lib/perl5/Module/Build/ConfigData.pm
@@ -167,44 +167,44 @@ do{ my $x = [
        {},
        {},
        {
-         'PPM_support' => {
-                            'description' => 'Generate PPM files for distributions'
-                          },
-         'HTML_support' => {
-                             'description' => 'Create HTML documentation',
-                             'requires' => {
-                                             'Pod::Html' => 0
-                                           }
-                           },
-         'inc_bundling_support' => {
-                                     'requires' => {
-                                                     'ExtUtils::Installed' => '1.999',
-                                                     'ExtUtils::Install' => '1.54'
-                                                   },
-                                     'description' => 'Bundle Module::Build in inc/'
-                                   },
-         'manpage_support' => {
-                                'requires' => {
-                                                'Pod::Man' => 0
-                                              },
-                                'description' => 'Create Unix man pages'
-                              },
+         'license_creation' => {
+                                 'requires' => {
+                                                 'Software::License' => '0.103009'
+                                               },
+                                 'description' => 'Create licenses automatically in distributions'
+                               },
          'dist_authoring' => {
                                'recommends' => {
-                                                 'Module::Signature' => '0.21',
-                                                 'Pod::Readme' => '0.04'
+                                                 'Pod::Readme' => '0.04',
+                                                 'Module::Signature' => '0.21'
                                                },
                                'description' => 'Create new distributions',
                                'requires' => {
                                                'Archive::Tar' => '1.09'
                                              }
                              },
-         'license_creation' => {
-                                 'requires' => {
-                                                 'Software::License' => '0.103009'
-                                               },
-                                 'description' => 'Create licenses automatically in distributions'
-                               }
+         'PPM_support' => {
+                            'description' => 'Generate PPM files for distributions'
+                          },
+         'inc_bundling_support' => {
+                                     'description' => 'Bundle Module::Build in inc/',
+                                     'requires' => {
+                                                     'ExtUtils::Install' => '1.54',
+                                                     'ExtUtils::Installed' => '1.999'
+                                                   }
+                                   },
+         'HTML_support' => {
+                             'description' => 'Create HTML documentation',
+                             'requires' => {
+                                             'Pod::Html' => 0
+                                           }
+                           },
+         'manpage_support' => {
+                                'description' => 'Create Unix man pages',
+                                'requires' => {
+                                                'Pod::Man' => 0
+                                              }
+                              }
        }
      ];
 $x; }
