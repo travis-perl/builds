acca831f3cab2afbafb84588e86e157d149cdbdf
Graham Knop <haarg@haarg.org>
2014-12-19 23:21:33 +0000

    local::lib cover for Perl 5.20.0 built for heads/master-0-gb7145cc

--- lib/perl5/Module/Build/ConfigData.pm
+++ lib/perl5/Module/Build/ConfigData.pm
@@ -167,44 +167,44 @@ do{ my $x = [
        {},
        {},
        {
-         'manpage_support' => {
-                                'requires' => {
-                                                'Pod::Man' => 0
-                                              },
-                                'description' => 'Create Unix man pages'
-                              },
-         'license_creation' => {
-                                 'requires' => {
-                                                 'Software::License' => '0.103009'
-                                               },
-                                 'description' => 'Create licenses automatically in distributions'
-                               },
+         'HTML_support' => {
+                             'requires' => {
+                                             'Pod::Html' => 0
+                                           },
+                             'description' => 'Create HTML documentation'
+                           },
+         'PPM_support' => {
+                            'description' => 'Generate PPM files for distributions'
+                          },
          'dist_authoring' => {
+                               'description' => 'Create new distributions',
                                'recommends' => {
                                                  'Module::Signature' => '0.21',
                                                  'Pod::Readme' => '0.04'
                                                },
                                'requires' => {
                                                'Archive::Tar' => '1.09'
-                                             },
-                               'description' => 'Create new distributions'
+                                             }
                              },
-         'HTML_support' => {
-                             'description' => 'Create HTML documentation',
-                             'requires' => {
-                                             'Pod::Html' => 0
-                                           }
-                           },
-         'PPM_support' => {
-                            'description' => 'Generate PPM files for distributions'
-                          },
+         'license_creation' => {
+                                 'requires' => {
+                                                 'Software::License' => '0.103009'
+                                               },
+                                 'description' => 'Create licenses automatically in distributions'
+                               },
          'inc_bundling_support' => {
                                      'requires' => {
-                                                     'ExtUtils::Installed' => '1.999',
-                                                     'ExtUtils::Install' => '1.54'
+                                                     'ExtUtils::Install' => '1.54',
+                                                     'ExtUtils::Installed' => '1.999'
                                                    },
                                      'description' => 'Bundle Module::Build in inc/'
-                                   }
+                                   },
+         'manpage_support' => {
+                                'requires' => {
+                                                'Pod::Man' => 0
+                                              },
+                                'description' => 'Create Unix man pages'
+                              }
        }
      ];
 $x; }
