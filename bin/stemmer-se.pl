844225ab7114a79ed2d477cc77775e400a761d0e
Graham Knop <haarg@haarg.org>
2014-12-19 22:44:12 +0000

    local::lib dbic for Perl 5.16.0 built for heads/master-0-gb7145cc

---
+++ bin/stemmer-se.pl
@@ -0,0 +1,24 @@
+#!/home/travis/perl5/perlbrew/perls/5.16.0/bin/perl 
+
+eval 'exec /home/travis/perl5/perlbrew/perls/5.16.0/bin/perl  -S $0 ${1+"$@"}'
+    if 0; # not running under some shell
+# $Id: stemmer.pl,v 1.1 2007/05/07 12:01:14 ask Exp $
+# $Source: /opt/CVS/SeSnowball/examples/stemmer.pl,v $
+# $Author: ask $
+# $HeadURL$
+# $Revision: 1.1 $
+# $Date: 2007/05/07 12:01:14 $
+use strict;
+use warnings;
+use Lingua::Stem::Snowball::Se;
+use vars qw($VERSION);
+$VERSION = 1.2;
+
+my $stemmer = Lingua::Stem::Snowball::Se->new(use_cache => 1);
+while (my $line = <>) {
+	chomp $line;
+	foreach my $word ((split m/\s+/xms, $line)) {
+		my $stemmed = $stemmer->stem($word);
+		print "$stemmed\n";
+	}
+}
