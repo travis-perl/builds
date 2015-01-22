2b33c0c3fc08db082f9298e25920193f6edb0c39
Automatic Dev Perl Updates <autobuild@autobuild>
2015-01-22 07:15:12 +0000

    Perl 5.16.3 built for heads/master-0-g214ef08

--- lib/5.16.3/x86_64-linux/CORE/config.h
+++ lib/5.16.3/x86_64-linux/CORE/config.h
@@ -9,9 +9,9 @@
 
 /* Package name      : perl5
  * Source directory  : .
- * Configuration time: Fri Dec 19 22:27:58 UTC 2014
+ * Configuration time: Thu Jan 22 06:56:54 UTC 2015
  * Configured by     : travis
- * Target system     : linux testing-worker-linux-8-1-9724-linux-6-44618263 2.6.32-042stab090.5 #1 smp sat jun 21 00:15:09 msk 2014 x86_64 x86_64 x86_64 gnulinux 
+ * Target system     : linux testing-worker-linux-11-1-31038-linux-16-47879109 2.6.32-042stab090.5 #1 smp sat jun 21 00:15:09 msk 2014 x86_64 x86_64 x86_64 gnulinux 
  */
 
 #ifndef _config_h_
