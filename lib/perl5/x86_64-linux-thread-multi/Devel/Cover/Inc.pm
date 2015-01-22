2969d0e077ba6bc1242b02acf576fddcfb1c6334
Automatic Dev Perl Updates <autobuild@autobuild>
2015-01-22 06:09:00 +0000

    local::lib cover for Perl 5.8.8-thr built for heads/master-0-g214ef08

--- lib/perl5/x86_64-linux-thread-multi/Devel/Cover/Inc.pm
+++ lib/perl5/x86_64-linux-thread-multi/Devel/Cover/Inc.pm
@@ -14,7 +14,7 @@ use warnings;
 
 our $VERSION      = "1.17";
 our $Perl_version = '5.008008';
-our $Base         = '/home/travis/.cpanm/work/1421900753.31598/Devel-Cover-1.17';
+our $Base         = '/home/travis/.cpanm/work/1421906023.31627/Devel-Cover-1.17';
 our @Inc          = qw( . /home/travis/.perlbrew/libs/5.8.8-thr@cover/lib/perl5 /home/travis/.perlbrew/libs/5.8.8-thr@cover/lib/perl5/x86_64-linux-thread-multi /home/travis/perl5/perlbrew/perls/5.8.8-thr/lib/5.8.8 /home/travis/perl5/perlbrew/perls/5.8.8-thr/lib/5.8.8/x86_64-linux-thread-multi /home/travis/perl5/perlbrew/perls/5.8.8-thr/lib/site_perl /home/travis/perl5/perlbrew/perls/5.8.8-thr/lib/site_perl/5.8.8 /home/travis/perl5/perlbrew/perls/5.8.8-thr/lib/site_perl/5.8.8/x86_64-linux-thread-multi );
 chomp (our $Perl  = <<'EOV');  # Careful with \\ in the path
 /home/travis/perl5/perlbrew/perls/5.8.8-thr/bin/perl
