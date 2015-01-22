f47ce1024d3425b5a473c15423dc5d1a9c06c8db
Automatic Dev Perl Updates <autobuild@autobuild>
2015-01-22 07:05:43 +0000

    local::lib cover for Perl 5.14.4-thr built for heads/master-0-g214ef08

--- lib/perl5/x86_64-linux-thread-multi/Devel/Cover/Inc.pm
+++ lib/perl5/x86_64-linux-thread-multi/Devel/Cover/Inc.pm
@@ -14,7 +14,7 @@ use warnings;
 
 our $VERSION      = "1.17";
 our $Perl_version = '5.014004';
-our $Base         = '/home/travis/.cpanm/work/1419027806.4433/Devel-Cover-1.17';
+our $Base         = '/home/travis/.cpanm/work/1421909687.4425/Devel-Cover-1.17';
 our @Inc          = qw( . /home/travis/.perlbrew/libs/5.14.4-thr@cover/lib/perl5 /home/travis/.perlbrew/libs/5.14.4-thr@cover/lib/perl5/x86_64-linux-thread-multi /home/travis/perl5/perlbrew/perls/5.14.4-thr/lib/5.14.4 /home/travis/perl5/perlbrew/perls/5.14.4-thr/lib/5.14.4/x86_64-linux-thread-multi /home/travis/perl5/perlbrew/perls/5.14.4-thr/lib/site_perl/5.14.4 /home/travis/perl5/perlbrew/perls/5.14.4-thr/lib/site_perl/5.14.4/x86_64-linux-thread-multi );
 chomp (our $Perl  = <<'EOV');  # Careful with \\ in the path
 /home/travis/perl5/perlbrew/perls/5.14.4-thr/bin/perl
