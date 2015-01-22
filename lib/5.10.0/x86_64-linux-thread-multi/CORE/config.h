c28e2cf11298001f701b868e49143f88cf1502c9
Automatic Dev Perl Updates <autobuild@autobuild>
2015-01-22 06:33:28 +0000

    Perl 5.10.0-thr built for heads/master-0-g214ef08

--- lib/5.10.0/x86_64-linux-thread-multi/CORE/config.h
+++ lib/5.10.0/x86_64-linux-thread-multi/CORE/config.h
@@ -13,9 +13,9 @@
 /*
  * Package name      : perl5
  * Source directory  : .
- * Configuration time: Thu Jan 22 04:39:05 UTC 2015
+ * Configuration time: Thu Jan 22 06:04:52 UTC 2015
  * Configured by     : travis
- * Target system     : linux testing-worker-linux-de84ae74-2-8732-linux-18-47875570 2.6.32-042stab090.5 #1 smp sat jun 21 00:15:09 msk 2014 x86_64 x86_64 x86_64 gnulinux 
+ * Target system     : linux testing-worker-linux-11-2-30004-linux-1-47879096 2.6.32-042stab090.5 #1 smp sat jun 21 00:15:09 msk 2014 x86_64 x86_64 x86_64 gnulinux 
  */
 
 #ifndef _config_h_
