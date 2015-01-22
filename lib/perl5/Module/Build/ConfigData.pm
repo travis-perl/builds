f2d8fb52c0a95cc66f059f0d3cfdb499a918e6a9
Automatic Dev Perl Updates <autobuild@autobuild>
2015-01-22 07:59:02 +0000

    local::lib cover for Perl 5.20.0-thr built for heads/master-0-g214ef08

--- lib/perl5/Module/Build/ConfigData.pm
+++ lib/perl5/Module/Build/ConfigData.pm
@@ -167,44 +167,45 @@ do{ my $x = [
        {},
        {},
        {
+         'license_creation' => {
+                                 'description' => 'Create licenses automatically in distributions',
+                                 'requires' => {
+                                                 'Software::License' => '0.103009'
+                                               }
+                               },
+         'dist_authoring' => {
+                               'recommends' => {
+                                                 'Module::Signature' => '0.21',
+                                                 'Pod::Readme' => '0.04'
+                                               },
+                               'description' => 'Create new distributions',
+                               'requires' => {
+                                               'Archive::Tar' => '1.09'
+                                             }
+                             },
+         'PPM_support' => {
+                            'description' => 'Generate PPM files for distributions'
+                          },
+         'inc_bundling_support' => {
+                                     'description' => 'Bundle Module::Build in inc/',
+                                     'requires' => {
+                                                     'inc::latest' => '0.5',
+                                                     'ExtUtils::Installed' => '1.999',
+                                                     'ExtUtils::Install' => '1.54'
+                                                   }
+                                   },
          'HTML_support' => {
                              'description' => 'Create HTML documentation',
                              'requires' => {
                                              'Pod::Html' => 0
                                            }
                            },
-         'inc_bundling_support' => {
-                                     'requires' => {
-                                                     'ExtUtils::Installed' => '1.999',
-                                                     'ExtUtils::Install' => '1.54'
-                                                   },
-                                     'description' => 'Bundle Module::Build in inc/'
-                                   },
-         'PPM_support' => {
-                            'description' => 'Generate PPM files for distributions'
-                          },
-         'dist_authoring' => {
-                               'requires' => {
-                                               'Archive::Tar' => '1.09'
-                                             },
-                               'description' => 'Create new distributions',
-                               'recommends' => {
-                                                 'Pod::Readme' => '0.04',
-                                                 'Module::Signature' => '0.21'
-                                               }
-                             },
          'manpage_support' => {
                                 'description' => 'Create Unix man pages',
                                 'requires' => {
                                                 'Pod::Man' => 0
                                               }
-                              },
-         'license_creation' => {
-                                 'requires' => {
-                                                 'Software::License' => '0.103009'
-                                               },
-                                 'description' => 'Create licenses automatically in distributions'
-                               }
+                              }
        }
      ];
 $x; }
