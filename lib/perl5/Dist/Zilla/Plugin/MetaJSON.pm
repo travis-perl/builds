33b5797c2ffdd51fb0d7eca5c5cb9d3f7b8347f2
Graham Knop <haarg@haarg.org>
2015-01-22 05:00:38 +0000

    local::lib dzil for Perl 5.10.1 built for heads/master-0-g0dc82f0

--- lib/perl5/Dist/Zilla/Plugin/MetaJSON.pm
+++ lib/perl5/Dist/Zilla/Plugin/MetaJSON.pm
@@ -1,6 +1,6 @@
 package Dist::Zilla::Plugin::MetaJSON;
 # ABSTRACT: produce a META.json
-$Dist::Zilla::Plugin::MetaJSON::VERSION = '5.029';
+$Dist::Zilla::Plugin::MetaJSON::VERSION = '5.031';
 use Moose;
 with 'Dist::Zilla::Role::FileGatherer';
 
@@ -114,7 +114,7 @@ Dist::Zilla::Plugin::MetaJSON - produce a META.json
 
 =head1 VERSION
 
-version 5.029
+version 5.031
 
 =head1 DESCRIPTION
 
@@ -157,7 +157,7 @@ Ricardo SIGNES <rjbs@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2014 by Ricardo SIGNES.
+This software is copyright (c) 2015 by Ricardo SIGNES.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
--- lib/perl5/Dist/Zilla/Prereqs.pm
+++ lib/perl5/Dist/Zilla/Prereqs.pm
@@ -1,6 +1,6 @@
 package Dist::Zilla::Prereqs;
 # ABSTRACT: the prerequisites of a Dist::Zilla distribution
-$Dist::Zilla::Prereqs::VERSION = '5.029';
+$Dist::Zilla::Prereqs::VERSION = '5.031';
 use Moose;
 use MooseX::Types::Moose qw(Bool HashRef);
 
@@ -146,7 +146,7 @@ Dist::Zilla::Prereqs - the prerequisites of a Dist::Zilla distribution
 
 =head1 VERSION
 
-version 5.029
+version 5.031
 
 =head1 DESCRIPTION
 
@@ -210,7 +210,7 @@ Ricardo SIGNES <rjbs@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2014 by Ricardo SIGNES.
+This software is copyright (c) 2015 by Ricardo SIGNES.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
