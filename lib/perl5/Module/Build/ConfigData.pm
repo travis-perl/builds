0198b86a3a46a18bd6a0b62c4e60d1e79b0b17e9
Graham Knop <haarg@haarg.org>
2015-02-03 06:52:39 +0000

    local::lib cover for Perl 5.21.8-thr built for heads/master-0-gc52cf15

--- lib/perl5/Module/Build/ConfigData.pm
+++ lib/perl5/Module/Build/ConfigData.pm
@@ -167,45 +167,45 @@ do{ my $x = [
        {},
        {},
        {
+         'PPM_support' => {
+                            'description' => 'Generate PPM files for distributions'
+                          },
+         'dist_authoring' => {
+                               'recommends' => {
+                                                 'Pod::Readme' => '0.04',
+                                                 'Module::Signature' => '0.21'
+                                               },
+                               'requires' => {
+                                               'Archive::Tar' => '1.09'
+                                             },
+                               'description' => 'Create new distributions'
+                             },
          'license_creation' => {
                                  'description' => 'Create licenses automatically in distributions',
                                  'requires' => {
                                                  'Software::License' => '0.103009'
                                                }
                                },
-         'manpage_support' => {
-                                'requires' => {
-                                                'Pod::Man' => 0
-                                              },
-                                'description' => 'Create Unix man pages'
-                              },
-         'PPM_support' => {
-                            'description' => 'Generate PPM files for distributions'
-                          },
          'HTML_support' => {
                              'requires' => {
                                              'Pod::Html' => 0
                                            },
                              'description' => 'Create HTML documentation'
                            },
-         'dist_authoring' => {
-                               'requires' => {
-                                               'Archive::Tar' => '1.09'
-                                             },
-                               'description' => 'Create new distributions',
-                               'recommends' => {
-                                                 'Pod::Readme' => '0.04',
-                                                 'Module::Signature' => '0.21'
-                                               }
-                             },
          'inc_bundling_support' => {
-                                     'description' => 'Bundle Module::Build in inc/',
                                      'requires' => {
                                                      'ExtUtils::Installed' => '1.999',
-                                                     'ExtUtils::Install' => '1.54',
-                                                     'inc::latest' => '0.5'
-                                                   }
-                                   }
+                                                     'inc::latest' => '0.5',
+                                                     'ExtUtils::Install' => '1.54'
+                                                   },
+                                     'description' => 'Bundle Module::Build in inc/'
+                                   },
+         'manpage_support' => {
+                                'requires' => {
+                                                'Pod::Man' => 0
+                                              },
+                                'description' => 'Create Unix man pages'
+                              }
        }
      ];
 $x; }
--- lib/perl5/x86_64-linux-thread-multi/Devel/Cover/Inc.pm
+++ lib/perl5/x86_64-linux-thread-multi/Devel/Cover/Inc.pm
@@ -14,7 +14,7 @@ use warnings;
 
 our $VERSION      = "1.17";
 our $Perl_version = '5.021008';
-our $Base         = '/home/travis/.cpanm/work/1421913288.31583/Devel-Cover-1.17';
+our $Base         = '/home/travis/.cpanm/work/1422945736.31573/Devel-Cover-1.17';
 our @Inc          = qw( . /home/travis/perl5/perlbrew/perls/5.21.8-thr/lib/5.21.8 /home/travis/perl5/perlbrew/perls/5.21.8-thr/lib/5.21.8/x86_64-linux-thread-multi /home/travis/perl5/perlbrew/perls/5.21.8-thr/lib/site_perl/5.21.8 /home/travis/perl5/perlbrew/perls/5.21.8-thr/lib/site_perl/5.21.8/x86_64-linux-thread-multi );
 chomp (our $Perl  = <<'EOV');  # Careful with \\ in the path
 /home/travis/perl5/perlbrew/perls/5.21.8-thr/bin/perl5.21.8
