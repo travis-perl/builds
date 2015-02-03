2855f02c987335eacc0ef4a7882c60cc9fb746c1
Graham Knop <haarg@haarg.org>
2015-02-03 06:52:41 +0000

    local::lib dbic for Perl 5.21.8-thr built for heads/master-0-gc52cf15

--- lib/perl5/Module/Build/ConfigData.pm
+++ lib/perl5/Module/Build/ConfigData.pm
@@ -167,45 +167,45 @@ do{ my $x = [
        {},
        {},
        {
+         'inc_bundling_support' => {
+                                     'requires' => {
+                                                     'ExtUtils::Install' => '1.54',
+                                                     'ExtUtils::Installed' => '1.999',
+                                                     'inc::latest' => '0.5'
+                                                   },
+                                     'description' => 'Bundle Module::Build in inc/'
+                                   },
+         'PPM_support' => {
+                            'description' => 'Generate PPM files for distributions'
+                          },
+         'manpage_support' => {
+                                'requires' => {
+                                                'Pod::Man' => 0
+                                              },
+                                'description' => 'Create Unix man pages'
+                              },
          'dist_authoring' => {
                                'description' => 'Create new distributions',
+                               'requires' => {
+                                               'Archive::Tar' => '1.09'
+                                             },
                                'recommends' => {
                                                  'Module::Signature' => '0.21',
                                                  'Pod::Readme' => '0.04'
-                                               },
-                               'requires' => {
-                                               'Archive::Tar' => '1.09'
-                                             }
+                                               }
                              },
-         'HTML_support' => {
-                             'description' => 'Create HTML documentation',
-                             'requires' => {
-                                             'Pod::Html' => 0
-                                           }
-                           },
-         'manpage_support' => {
-                                'description' => 'Create Unix man pages',
-                                'requires' => {
-                                                'Pod::Man' => 0
-                                              }
-                              },
-         'inc_bundling_support' => {
-                                     'description' => 'Bundle Module::Build in inc/',
-                                     'requires' => {
-                                                     'inc::latest' => '0.5',
-                                                     'ExtUtils::Installed' => '1.999',
-                                                     'ExtUtils::Install' => '1.54'
-                                                   }
-                                   },
          'license_creation' => {
-                                 'description' => 'Create licenses automatically in distributions',
                                  'requires' => {
                                                  'Software::License' => '0.103009'
-                                               }
+                                               },
+                                 'description' => 'Create licenses automatically in distributions'
                                },
-         'PPM_support' => {
-                            'description' => 'Generate PPM files for distributions'
-                          }
+         'HTML_support' => {
+                             'requires' => {
+                                             'Pod::Html' => 0
+                                           },
+                             'description' => 'Create HTML documentation'
+                           }
        }
      ];
 $x; }
