cd805aa1142c986b54bb2ebf14b2f30dec449e31
Automatic Dev Perl Updates <autobuild@autobuild>
2015-02-22 00:16:41 +0000

    local::lib dzil for Perl 5.21.9-thr built for heads/master-0-g28033d7

---
+++ lib/perl5/Dist/Zilla/MintingProfile/Default.pm
@@ -0,0 +1,72 @@
+package Dist::Zilla::MintingProfile::Default;
+# ABSTRACT: Default minting profile provider
+$Dist::Zilla::MintingProfile::Default::VERSION = '5.032';
+use Moose;
+with 'Dist::Zilla::Role::MintingProfile::ShareDir';
+
+use namespace::autoclean;
+
+use Dist::Zilla::Util;
+use Path::Class;
+
+#pod =head1 DESCRIPTION
+#pod
+#pod Default minting profile provider.
+#pod
+#pod This provider looks first in the F<~/.dzil/profiles/$profile_name> directory,
+#pod if not found it looks among the default profiles shipped with Dist::Zilla.
+#pod
+#pod =cut
+
+around profile_dir => sub {
+  my ($orig, $self, $profile_name) = @_;
+
+  $profile_name ||= 'default';
+
+  # shouldn't look in user's config when testing
+  if (!$ENV{DZIL_TESTING}) {
+    my $profile_dir = Dist::Zilla::Util->_global_config_root
+                    ->subdir('profiles', $profile_name);
+
+    return $profile_dir if -d $profile_dir;
+  }
+
+  return $self->$orig($profile_name);
+};
+
+__PACKAGE__->meta->make_immutable;
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Dist::Zilla::MintingProfile::Default - Default minting profile provider
+
+=head1 VERSION
+
+version 5.032
+
+=head1 DESCRIPTION
+
+Default minting profile provider.
+
+This provider looks first in the F<~/.dzil/profiles/$profile_name> directory,
+if not found it looks among the default profiles shipped with Dist::Zilla.
+
+=head1 AUTHOR
+
+Ricardo SIGNES <rjbs@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Ricardo SIGNES.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
---
+++ lib/perl5/Dist/Zilla/Role/Stash.pm
@@ -0,0 +1,53 @@
+package Dist::Zilla::Role::Stash;
+# ABSTRACT: something that stores options or data for later reference
+$Dist::Zilla::Role::Stash::VERSION = '5.032';
+use Moose::Role;
+
+use namespace::autoclean;
+
+sub register_component {
+  my ($class, $name, $arg, $section) = @_;
+
+  # $self->log_debug([ 'online, %s v%s', $self->meta->name, $version ]);
+  my $entry = $class->stash_from_config($name, $arg, $section);
+
+  $section->sequence->assembler->register_stash($name, $entry);
+
+  return;
+}
+
+sub stash_from_config {
+  my ($class, $name, $arg, $section) = @_;
+
+  my $self = $class->new($arg);
+  return $self;
+}
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Dist::Zilla::Role::Stash - something that stores options or data for later reference
+
+=head1 VERSION
+
+version 5.032
+
+=head1 AUTHOR
+
+Ricardo SIGNES <rjbs@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Ricardo SIGNES.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
