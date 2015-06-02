04c771e638b91234ea293c3b53fa2f235037fda7
Graham Knop <haarg@haarg.org>
2015-06-02 21:48:42 +0000

    local::lib dzil for Perl 5.22.0-thr built for heads/master-0-g6c13306

---
+++ lib/perl5/Dist/Zilla/Stash/Rights.pm
@@ -0,0 +1,54 @@
+package Dist::Zilla::Stash::Rights;
+# ABSTRACT: a stash of your default licensing terms
+$Dist::Zilla::Stash::Rights::VERSION = '5.036';
+use Moose;
+with 'Dist::Zilla::Role::Stash';
+
+use namespace::autoclean;
+
+has license_class => (
+  is  => 'ro',
+  isa => 'Str',
+  required => 1,
+);
+
+has copyright_holder => (
+  is  => 'ro',
+  isa => 'Str',
+  required => 1,
+);
+
+has copyright_year => (
+  is  => 'ro',
+  isa => 'Int',
+);
+
+__PACKAGE__->meta->make_immutable;
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Dist::Zilla::Stash::Rights - a stash of your default licensing terms
+
+=head1 VERSION
+
+version 5.036
+
+=head1 AUTHOR
+
+Ricardo SIGNES <rjbs@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Ricardo SIGNES.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
