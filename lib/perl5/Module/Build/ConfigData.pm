f0c1c5952ad1a3269ad74a93b2e2ac39db152e3b
Graham Knop <haarg@haarg.org>
2015-07-21 17:18:23 +0000

    local::lib moo for Perl 5.22.0 built for heads/master-0-g60be148

--- lib/perl5/Module/Build/ConfigData.pm
+++ lib/perl5/Module/Build/ConfigData.pm
@@ -167,38 +167,38 @@ do{ my $x = [
        {},
        {},
        {
-         'dist_authoring' => {
-                               'description' => 'Create new distributions',
-                               'requires' => {
-                                               'Archive::Tar' => '1.09'
-                                             },
-                               'recommends' => {
-                                                 'Pod::Readme' => '0.04',
-                                                 'Module::Signature' => '0.21'
-                                               }
-                             },
+         'HTML_support' => {
+                             'description' => 'Create HTML documentation',
+                             'requires' => {
+                                             'Pod::Html' => 0
+                                           }
+                           },
          'PPM_support' => {
                             'description' => 'Generate PPM files for distributions'
                           },
+         'dist_authoring' => {
+                               'description' => 'Create new distributions',
+                               'recommends' => {
+                                                 'Module::Signature' => '0.21',
+                                                 'Pod::Readme' => '0.04'
+                                               },
+                               'requires' => {
+                                               'Archive::Tar' => '1.09'
+                                             }
+                             },
          'inc_bundling_support' => {
                                      'description' => 'Bundle Module::Build in inc/',
                                      'requires' => {
+                                                     'ExtUtils::Install' => '1.54',
                                                      'ExtUtils::Installed' => '1.999',
-                                                     'inc::latest' => '0.5',
-                                                     'ExtUtils::Install' => '1.54'
+                                                     'inc::latest' => '0.5'
                                                    }
                                    },
-         'HTML_support' => {
-                             'requires' => {
-                                             'Pod::Html' => 0
-                                           },
-                             'description' => 'Create HTML documentation'
-                           },
          'license_creation' => {
+                                 'description' => 'Create licenses automatically in distributions',
                                  'requires' => {
                                                  'Software::License' => '0.103009'
-                                               },
-                                 'description' => 'Create licenses automatically in distributions'
+                                               }
                                },
          'manpage_support' => {
                                 'description' => 'Create Unix man pages',
