71a54a7b138532102a510b548762f7b0d8f68fb1
Automatic Dev Perl Updates <autobuild@autobuild>
2015-08-21 00:14:57 +0000

    local::lib cover for Perl 5.22.0 built for heads/master-0-g8196241

--- lib/perl5/x86_64-linux/Devel/Cover/Inc.pm
+++ lib/perl5/x86_64-linux/Devel/Cover/Inc.pm
@@ -14,7 +14,7 @@ use warnings;
 
 our $VERSION      = "1.20";
 our $Perl_version = '5.022000';
-our $Base         = '/home/travis/.cpanm/work/1437500565.31506/Devel-Cover-1.20';
+our $Base         = '/home/travis/.cpanm/work/1440115587.31607/Devel-Cover-1.20';
 our @Inc          = qw( . /home/travis/perl5/perlbrew/perls/5.22.0/lib/5.22.0 /home/travis/perl5/perlbrew/perls/5.22.0/lib/5.22.0/x86_64-linux /home/travis/perl5/perlbrew/perls/5.22.0/lib/site_perl/5.22.0 /home/travis/perl5/perlbrew/perls/5.22.0/lib/site_perl/5.22.0/x86_64-linux );
 chomp (our $Perl  = <<'EOV');  # Careful with \\ in the path
 /home/travis/perl5/perlbrew/perls/5.22.0/bin/perl
