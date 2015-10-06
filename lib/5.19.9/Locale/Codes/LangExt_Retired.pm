1a81ab411292b75b814fa4b1a53614c894143e21
Automatic Dev Perl Updates <autobuild@autobuild>
2015-10-06 00:27:22 +0000

    Perl 5.19.9-thr built for heads/master-0-gafa112b

---
+++ lib/5.19.9/Locale/Codes/LangExt_Retired.pm
@@ -0,0 +1,23 @@
+package #
+Locale::Codes::LangExt_Retired;
+
+# This file was automatically generated.  Any changes to this file will
+# be lost the next time 'deprecate_codes' is run.
+#    Generated on: Fri Jan 24 09:18:46 EST 2014
+
+use strict;
+require 5.006;
+use warnings;
+use utf8;
+
+our($VERSION);
+$VERSION='3.29';
+
+$Locale::Codes::Retired{'langext'}{'alpha'}{'code'} = {
+};
+
+$Locale::Codes::Retired{'langext'}{'alpha'}{'name'} = {
+};
+
+
+1;
