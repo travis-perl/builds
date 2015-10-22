5a7cca89ae3fc94a6bde418bf96f4ac1de1ec8ff
Automatic Dev Perl Updates <autobuild@autobuild>
2015-10-22 00:14:47 +0000

    local::lib cat for Perl 5.22.0-thr built for heads/master-0-g0ba22ce

--- lib/perl5/x86_64-linux-thread-multi/MooseX/Role/WithOverloading/Meta/Role/Composite.pm
+++ lib/perl5/x86_64-linux-thread-multi/MooseX/Role/WithOverloading/Meta/Role/Composite.pm
@@ -1,6 +1,7 @@
 package MooseX::Role::WithOverloading::Meta::Role::Composite;
-# ABSTRACT: Role for composite roles which support overloading
-$MooseX::Role::WithOverloading::Meta::Role::Composite::VERSION = '0.16';
+
+our $VERSION = '0.17';
+
 use Moose::Role;
 use Moose::Util::MetaRole;
 use aliased 'MooseX::Role::WithOverloading::Meta::Role::Application::Composite::ToClass';
@@ -8,12 +9,6 @@ use aliased 'MooseX::Role::WithOverloading::Meta::Role::Application::Composite::
 use aliased 'MooseX::Role::WithOverloading::Meta::Role::Application::Composite::ToInstance';
 
 use namespace::autoclean;
-
-#pod =method apply_params
-#pod
-#pod Wrapped method to apply various metaclass roles to aid with role composition.
-#pod
-#pod =cut
 
 around apply_params => sub {
     my ($next, $self, @args) = @_;
@@ -28,46 +23,3 @@ around apply_params => sub {
 };
 
 1;
-
-__END__
-
-=pod
-
-=encoding UTF-8
-
-=head1 NAME
-
-MooseX::Role::WithOverloading::Meta::Role::Composite - Role for composite roles which support overloading
-
-=head1 VERSION
-
-version 0.16
-
-=head1 METHODS
-
-=head2 apply_params
-
-Wrapped method to apply various metaclass roles to aid with role composition.
-
-=head1 AUTHORS
-
-=over 4
-
-=item *
-
-Florian Ragwitz <rafl@debian.org>
-
-=item *
-
-Tomas Doran <bobtfish@bobtfish.net>
-
-=back
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is copyright (c) 2009 by Florian Ragwitz.
-
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
-
-=cut
