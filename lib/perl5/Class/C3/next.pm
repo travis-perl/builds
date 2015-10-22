129ebad13396a2992344353a80149018dbe13efc
Automatic Dev Perl Updates <autobuild@autobuild>
2015-10-22 00:14:49 +0000

    local::lib dbic for Perl 5.22.0-thr built for heads/master-0-g0ba22ce

--- lib/perl5/Class/C3/next.pm
+++ lib/perl5/Class/C3/next.pm
@@ -7,7 +7,7 @@ no warnings 'redefine'; # for 00load.t w/ core support
 
 use Scalar::Util 'blessed';
 
-our $VERSION = '0.28';
+our $VERSION = '0.30';
 
 our %METHOD_CACHE;
 
@@ -66,7 +66,7 @@ use strict;
 use warnings;
 no warnings 'redefine'; # for 00load.t w/ core support
 
-our $VERSION = '0.28';
+our $VERSION = '0.30';
 
 sub method { (next::method($_[0]) || return)->(@_) }
 
