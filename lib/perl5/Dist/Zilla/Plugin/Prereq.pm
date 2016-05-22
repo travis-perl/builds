4cc8ba8b1a22d19949e1456c1fba7d8393c0425d
Automatic Dev Perl Updates <autobuild@autobuild>
2016-05-22 00:10:44 +0000

    local::lib dzil for Perl 5.25.1 built for heads/master-0-g1280af8

---
+++ lib/perl5/Dist/Zilla/Plugin/Prereq.pm
@@ -0,0 +1,64 @@
+package Dist::Zilla::Plugin::Prereq;
+# ABSTRACT: (DEPRECATED) the old name of the Prereqs plugin
+$Dist::Zilla::Plugin::Prereq::VERSION = '5.047';
+use Moose;
+extends 'Dist::Zilla::Plugin::Prereqs';
+
+use namespace::autoclean;
+
+#pod =head1 SYNOPSIS
+#pod
+#pod This plugin extends C<[Prereqs]> and adds nothing.  It is the old name for
+#pod Prereqs, and will be removed in a few versions.
+#pod
+#pod =head1 SEE ALSO
+#pod
+#pod Dist::Zilla plugins: L<Prereqs|Dist::Zilla::Plugin::Prereqs>.
+#pod
+#pod =cut
+
+before register_component => sub {
+  die "[Prereq] is incompatible with Dist::Zilla >= v5; replace it with [Prereqs] (note the
+  's')"
+    if Dist::Zilla->VERSION >= 5;
+  warn "!!! [Prereq] will be removed in Dist::Zilla v5; replace it with [Prereqs] (note the 's')\n";
+};
+
+__PACKAGE__->meta->make_immutable;
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Dist::Zilla::Plugin::Prereq - (DEPRECATED) the old name of the Prereqs plugin
+
+=head1 VERSION
+
+version 5.047
+
+=head1 SYNOPSIS
+
+This plugin extends C<[Prereqs]> and adds nothing.  It is the old name for
+Prereqs, and will be removed in a few versions.
+
+=head1 SEE ALSO
+
+Dist::Zilla plugins: L<Prereqs|Dist::Zilla::Plugin::Prereqs>.
+
+=head1 AUTHOR
+
+Ricardo SIGNES 🎃 <rjbs@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2016 by Ricardo SIGNES.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
---
+++ lib/perl5/Dist/Zilla/Role/StubBuild.pm
@@ -0,0 +1,35 @@
+package Dist::Zilla::Role::StubBuild;
+# ABSTRACT: provides an empty BUILD methods
+$Dist::Zilla::Role::StubBuild::VERSION = '5.047';
+use Moose::Role;
+
+sub BUILD {}
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Dist::Zilla::Role::StubBuild - provides an empty BUILD methods
+
+=head1 VERSION
+
+version 5.047
+
+=head1 AUTHOR
+
+Ricardo SIGNES 🎃 <rjbs@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2016 by Ricardo SIGNES.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
