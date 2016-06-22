2cd6b96d218c1ee782c1bfdd44face3ec4f681ea
Automatic Dev Perl Updates <autobuild@autobuild>
2016-06-22 00:12:19 +0000

    local::lib dzil for Perl 5.25.2-thr built for heads/master-0-g39cee38

---
+++ lib/perl5/Dist/Zilla/Role/ConfigDumper.pm
@@ -0,0 +1,37 @@
+package Dist::Zilla::Role::ConfigDumper;
+# ABSTRACT: something that can dump its (public, simplified) configuration
+$Dist::Zilla::Role::ConfigDumper::VERSION = '6.005';
+use Moose::Role;
+
+use namespace::autoclean;
+
+sub dump_config { return {}; }
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Dist::Zilla::Role::ConfigDumper - something that can dump its (public, simplified) configuration
+
+=head1 VERSION
+
+version 6.005
+
+=head1 AUTHOR
+
+Ricardo SIGNES 😏 <rjbs@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2016 by Ricardo SIGNES.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
