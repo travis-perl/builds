fb0aaff076f3dfe4463951c492c250bdb29777cb
mohawk2 <mohawk2@users.noreply.github.com>
2020-03-02 04:12:34 +0000

    Perl 5.24.0-thr built for heads/master-0-gfdd9703

--- lib/5.24.0/x86_64-linux-thread-multi/Config_heavy.pl
+++ lib/5.24.0/x86_64-linux-thread-multi/Config_heavy.pl
@@ -78,9 +78,9 @@ sub header_files {
 #
 ## Package name      : perl5
 ## Source directory  : .
-## Configuration time: Mon Mar  2 03:29:47 UTC 2020
+## Configuration time: Mon Mar  2 04:03:26 UTC 2020
 ## Configured by     : travis
-## Target system     : linux travis-job-81f34dc3-43fc-400b-8b3b-e287e4f46584 4.15.0-1028-gcp #29~16.04.1-ubuntu smp tue feb 12 16:31:10 utc 2019 x86_64 x86_64 x86_64 gnulinux 
+## Target system     : linux travis-job-9f4c3556-16b8-471a-90ff-71c584ff6b16 4.15.0-1028-gcp #29~16.04.1-ubuntu smp tue feb 12 16:31:10 utc 2019 x86_64 x86_64 x86_64 gnulinux 
 #
 #: Configure command line arguments.
 #
@@ -203,8 +203,8 @@ ccname='gcc'
 ccsymbols=''
 ccversion=''
 cf_by='travis'
-cf_email='travis@travis-job-81f34dc3-43fc-400b-8b3b-e287e4f46584.(none)'
-cf_time='Mon Mar  2 03:29:47 UTC 2020'
+cf_email='travis@travis-job-9f4c3556-16b8-471a-90ff-71c584ff6b16.(none)'
+cf_time='Mon Mar  2 04:03:26 UTC 2020'
 charbits='8'
 charsize='1'
 chgrp=''
@@ -1068,8 +1068,8 @@ multiarch='undef'
 mv=''
 myarchname='x86_64-linux'
 mydomain='.(none)'
-myhostname='travis-job-81f34dc3-43fc-400b-8b3b-e287e4f46584'
-myuname='linux travis-job-81f34dc3-43fc-400b-8b3b-e287e4f46584 4.15.0-1028-gcp #29~16.04.1-ubuntu smp tue feb 12 16:31:10 utc 2019 x86_64 x86_64 x86_64 gnulinux '
+myhostname='travis-job-9f4c3556-16b8-471a-90ff-71c584ff6b16'
+myuname='linux travis-job-9f4c3556-16b8-471a-90ff-71c584ff6b16 4.15.0-1028-gcp #29~16.04.1-ubuntu smp tue feb 12 16:31:10 utc 2019 x86_64 x86_64 x86_64 gnulinux '
 n='-n'
 need_va_copy='define'
 netdb_hlen_type='size_t'
@@ -1109,7 +1109,7 @@ perl5='/usr/bin/perl'
 perl='perl'
 perl_patchlevel=''
 perl_static_inline='static __inline__'
-perladmin='travis@travis-job-81f34dc3-43fc-400b-8b3b-e287e4f46584.(none)'
+perladmin='travis@travis-job-9f4c3556-16b8-471a-90ff-71c584ff6b16.(none)'
 perllibs='-lpthread -lnsl -ldl -lm -lcrypt -lutil -lc'
 perlpath='/home/travis/perl5/perlbrew/perls/5.24.0-thr/bin/perl'
 pg='pg'
