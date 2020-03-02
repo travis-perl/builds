923d393932a03cf6a4366e2e6d3751536976c94c
mohawk2 <mohawk2@users.noreply.github.com>
2020-03-02 04:45:58 +0000

    Perl 5.28.2-thr built for heads/master-0-gfdd9703

---
+++ lib/5.28.2/x86_64-linux-thread-multi/Storable/Limit.pm
@@ -0,0 +1,6 @@
+# bisected by stacksize
+$Storable::recursion_limit = 13635
+  unless defined $Storable::recursion_limit;
+$Storable::recursion_limit_hash = 8174
+  unless defined $Storable::recursion_limit_hash;
+1;
