56468165e3f88ac503934d08d8c8a11154f5ee07
Automatic Dev Perl Updates <autobuild@autobuild>
2020-03-08 00:09:53 +0000

    Perl 5.31.9 built for heads/master-0-ge67d91d

--- lib/5.31.9/x86_64-linux/CORE/config.h
+++ lib/5.31.9/x86_64-linux/CORE/config.h
@@ -9,9 +9,9 @@
 
 /* Package name      : perl5
  * Source directory  : .
- * Configuration time: Mon Mar  2 04:43:51 UTC 2020
+ * Configuration time: Sun Mar  8 00:01:01 UTC 2020
  * Configured by     : travis
- * Target system     : linux travis-job-7a8f5bfe-22d8-403e-8093-45ac8c555854 4.15.0-1028-gcp #29~16.04.1-ubuntu smp tue feb 12 16:31:10 utc 2019 x86_64 x86_64 x86_64 gnulinux 
+ * Target system     : linux travis-job-d7a45175-8cbe-46bd-aa65-79e88aee515d 4.15.0-1028-gcp #29~16.04.1-ubuntu smp tue feb 12 16:31:10 utc 2019 x86_64 x86_64 x86_64 gnulinux 
  */
 
 #ifndef _config_h_
