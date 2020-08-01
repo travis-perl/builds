42d93135b03f3c67934a5ed797877447d032170e
Ed J <mohawk2@users.noreply.github.com>
2020-08-01 01:44:13 +0000

    Perl 5.31.9-thr built for heads/master-0-gc068292

--- lib/5.31.9/x86_64-linux-thread-multi/CORE/config.h
+++ lib/5.31.9/x86_64-linux-thread-multi/CORE/config.h
@@ -9,9 +9,9 @@
 
 /* Package name      : perl5
  * Source directory  : .
- * Configuration time: Sun Mar  8 00:09:28 UTC 2020
+ * Configuration time: Sat Aug  1 01:28:23 UTC 2020
  * Configured by     : travis
- * Target system     : linux travis-job-d100062e-06d0-4491-b922-2b58ac324d2c 4.15.0-1028-gcp #29~16.04.1-ubuntu smp tue feb 12 16:31:10 utc 2019 x86_64 x86_64 x86_64 gnulinux 
+ * Target system     : linux travis-job-75f314ca-e006-492d-9fb6-69d9a50dbfe2 4.15.0-1028-gcp #29~16.04.1-ubuntu smp tue feb 12 16:31:10 utc 2019 x86_64 x86_64 x86_64 gnulinux 
  */
 
 #ifndef _config_h_
