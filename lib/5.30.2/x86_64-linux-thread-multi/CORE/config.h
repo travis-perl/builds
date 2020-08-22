6a6ad0264b277b43c943ea0e1d27b5f2882d3b0c
Automatic Dev Perl Updates <autobuild@autobuild>
2020-08-22 00:10:39 +0000

    Perl 5.30.2-thr built for heads/master-0-g67270db

--- lib/5.30.2/x86_64-linux-thread-multi/CORE/config.h
+++ lib/5.30.2/x86_64-linux-thread-multi/CORE/config.h
@@ -9,9 +9,9 @@
 
 /* Package name      : perl5
  * Source directory  : .
- * Configuration time: Sat Aug  1 01:14:30 UTC 2020
+ * Configuration time: Sat Aug 22 00:01:09 UTC 2020
  * Configured by     : travis
- * Target system     : linux travis-job-3737a5ab-5be6-4bc3-b5e7-f3e74df79d86 4.15.0-1028-gcp #29~16.04.1-ubuntu smp tue feb 12 16:31:10 utc 2019 x86_64 x86_64 x86_64 gnulinux 
+ * Target system     : linux travis-job-2b9f9ae8-8803-4155-94c2-3fd461a23bad 4.15.0-1028-gcp #29~16.04.1-ubuntu smp tue feb 12 16:31:10 utc 2019 x86_64 x86_64 x86_64 gnulinux 
  */
 
 #ifndef _config_h_
