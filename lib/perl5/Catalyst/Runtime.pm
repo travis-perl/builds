6a8ff62ba61a828a6a97bde398ab4cd156a7e16e
Automatic Dev Perl Updates <autobuild@autobuild>
2021-02-21 00:20:33 +0000

    local::lib cat for Perl 5.33.7 built for heads/master-0-ge6e4dc0

---
+++ lib/perl5/Catalyst/Runtime.pm
@@ -0,0 +1,34 @@
+package Catalyst::Runtime;
+
+use strict;
+use warnings;
+
+BEGIN { require 5.008003; }
+
+our $VERSION = '5.90128';
+$VERSION =~ tr/_//d;
+
+=head1 NAME
+
+Catalyst::Runtime - The Catalyst Framework Runtime
+
+=head1 SYNOPSIS
+
+See L<Catalyst>.
+
+=head1 DESCRIPTION
+
+This is the primary class for the Catalyst-Runtime distribution, version 5.80.
+
+=head1 AUTHORS & COPYRIGHT
+
+Catalyst Contributors, see Catalyst.pm
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
+
+1;
