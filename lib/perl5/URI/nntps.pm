3dec07d764faa3c2b6cc0a828dd1848e14229f4b
Automatic Dev Perl Updates <autobuild@autobuild>
2021-03-22 00:17:15 +0000

    local::lib cover for Perl 5.32.0-thr built for heads/master-0-g2cdb38d

--- lib/perl5/URI/nntps.pm
+++ lib/perl5/URI/nntps.pm
@@ -1,11 +1,11 @@
-package URI::snews;  # draft-gilman-news-url-01
+package URI::nntps;
 
 use strict;
 use warnings;
 
 our $VERSION = '5.09';
 
-use parent 'URI::news';
+use parent 'URI::nntp';
 
 sub default_port { 563 }
 
